18d4e78fc6cb31a8cb84497a1530d5d28870e518
Kauan Ortiz <kauan.ortizcc@gmail.com>
2026-04-13 17:41:21 -0300

    Operações implementadas

--- Calculadora.c
+++ Calculadora.c
@@ -1,29 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int soma(int a, int b){
+int sum(int a, int b){
 
     return a + b;
 }
 
-int subtracao(int a, int b){
+int sub(int a, int b){
 
     return a - b;
 }
 
-float divisao(int a, int b){
+float division(int a, int b){
 
-    return a / b;
+    return (float) a / b;
 }
 
-int multiplicacao(int a, int b){
+int multi(int a, int b){
 
     return a * b;
 }
 
 int main(){
 
+    int a, b, soma, subtracao, multiplicacao;
+    float divisao;
+
+    printf("Digite o primeiro inteiro: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo inteiro (Necessita ser diferente de 0): ");
+    scanf("%d", &b);
+
+    while(b == 0){       
+        printf("Valor inválido!\nDigite novamente: ");
+        scanf("%d", &b);
+    }
+
+    soma = sum(a, b);
+    subtracao = sub(a, b);
+    divisao = division(a, b);
+    multiplicacao = multi(a, b);
+
+    printf("Soma: %d\n", soma);
+    printf("Subtração: %d\n", subtracao);
+    printf("Divisão: %.2f\n", divisao);
+    printf("Multiplicação: %d\n", multiplicacao);
 
     return 0;
 }
